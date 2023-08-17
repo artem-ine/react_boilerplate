ffc6030138e559646e0f051ca5466aa57c8da7c3
ezekiel ravidat <ezekiel.ravidat@yahoo.com>
2023-08-17 14:34:44 +0200

    notif

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,13 +8,9 @@ function Footer() {
     >
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          <Link
-            to={{ pathname: "https://github.com/artem-ine/" }}
-            target="_blank"
-            className="flex items-center"
-          >
+          <a href="https://github.com/artem-ine/" className="flex items-center">
             artem-ine
-          </Link>
+          </a>
           All Rights Reserved. © 2023
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
