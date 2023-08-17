54182ece8354c60130548b20d95758b1a65e17f7
ezekiel ravidat <ezekiel.ravidat@yahoo.com>
2023-08-17 13:14:38 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <Footer />
     </>
   );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,8 @@
 function Home() {
   return (
     <div id="main" className="flex justify-center pt-10">
-      <h1 className="text-xl font-bold underline flex">
-        Welcome to your boilerplate.
+      <h1 className="text-xl font-bold flex">
+        Welcome to artemine's boilerplate.
       </h1>
     </div>
   );
