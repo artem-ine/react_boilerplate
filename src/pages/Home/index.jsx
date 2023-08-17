b26af200ee754d9c7a93c61f748deec8b075101d
ezekiel ravidat <ezekiel.ravidat@yahoo.com>
2023-08-17 12:41:48 +0200

    first push

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div id="main" className="flex justify-center pt-10">
+      <h1 className="text-xl font-bold underline flex">
+        Welcome to your boilerplate.
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
